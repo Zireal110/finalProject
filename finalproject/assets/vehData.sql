41096796298dc9709f111efea16056f5f4bef153
Zireal110 <77451895+Zireal110@users.noreply.github.com>
2021-01-14 17:42:43 +0200

    Add files via upload

---
+++ finalproject/assets/vehData.sql
@@ -0,0 +1,11 @@
+INSERT INTO cars 
+VALUES
+
+    (null,"Lamborghini Urus",203995,"Lamborghini-Urus.png",10,6),
+    (null,"Huracán Evo",261274,"Lamborghini-Huracán-Evo.png",10,6),
+    (null,"Huracán Evo Spyder",247400,"Lamborghini-Huracán-Evo-Spyder.png",10,6),
+    (null,"Aventador S",417650,"Lamborghini-Aventador-S.png",10,6),
+    (null,"Aventador S Roadster",467442,"Lamborghini-Aventador-LP740-4-S-Roadster.png",10,6),
+    (null,"Lamborghini Gallardo LP 570-4 Superleggera",157552,"Lamborghini-Gallardo-LP570-4-Superleggera.png",10,6);
+
+    
